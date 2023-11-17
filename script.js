73ae9a34db9b97945a0139a99047cc21c906bf2b
Shital Bhosle <94833435+shitalbhosle@users.noreply.github.com>
2023-11-17 20:08:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const text = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
+
+
+btn.addEventListener("click", function(){
+	text.innerHTML = "Entered Metaverse";
+})
